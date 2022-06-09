a23c250f825719f19d033280e9db462544f64c91
Andrea Mannocci <andremann@libero.it>
2022-06-09 15:02:07 +0200

    PC and new deadline

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("June 17, 2022 23:59:59 UTC-12").getTime();
+var countDownDate = new Date("June 26, 2022 23:59:59 UTC-12").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
